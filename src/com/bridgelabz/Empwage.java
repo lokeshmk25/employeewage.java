f17420ba32c29e724e1a81f290b8aa899c25571f
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-05 13:24:31 +0530

    added:switch case

--- src/com/bridgelabz/Empwage.java
+++ src/com/bridgelabz/Empwage.java
@@ -1,23 +1,28 @@
 package com.bridgelabz;
 
 public class Empwage {
-            public static void main(String[] args) {
+    //constants
+    public static final int PART_TIME = 1;
+    public static final int FULL_TIME = 2;
+    public static final int EMP_WAGE_PER_HR = 20;
 
-                System.out.println("Welcome to Bridgelabz EmployeeWageBuilder");
-//constant
-                int PART_TIME = 1;
-                int FULL_TIME=2;
-                int EMP_WAGE_PER_HR=20;
-                int calcWage=0;
-                int emphrs=0;
-                double empCheck = (int) Math.floor(Math.random() * 10) % 3;
-                if (empCheck == FULL_TIME)
-                    emphrs = 8;
-                else if (empCheck == PART_TIME )
-                    emphrs = 4;
-                calcWage = emphrs * EMP_WAGE_PER_HR;
-                System.out.println("Employeee daily wage =" +calcWage+ "RS");
-            }
+    public static void main(String[] arg) {
+        int calcWage = 0;
+        int emphrs = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case FULL_TIME:
+                emphrs = 8;
+                break;
+            case PART_TIME:
+                emphrs = 4;
+                break;
+            default:
+                emphrs = 0;
         }
+        calcWage = emphrs * EMP_WAGE_PER_HR;
+        System.out.println("Employeee daily wage =" + calcWage + "rs");
+    }
+}
 
 
