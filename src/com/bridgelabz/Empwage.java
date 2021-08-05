741a07286e4495615ea28f14f1ade0852218b302
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-05 12:07:49 +0530

    modified:employee wage

--- src/com/bridgelabz/Empwage.java
+++ src/com/bridgelabz/Empwage.java
@@ -6,12 +6,17 @@ public class Empwage {
                 System.out.println("Welcome to Bridgelabz EmployeeWageBuilder");
 //constant
                 int IS_PRESENT = 1;
-
+                int FULL_TIME=8;
+                int EMP_WAGE_PER_HR=20;
                 int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-                if (empCheck == IS_PRESENT)
+                int calcWage=0;
+                if (empCheck == IS_PRESENT) {
                     System.out.println(" Employee is Present");
+                    calcWage = FULL_TIME * EMP_WAGE_PER_HR;
+                }
                 else
                     System.out.println(" Employee is Absent");
+                System.out.println("Employeee daily wage =" +calcWage+ "RS");
             }
         }
 
