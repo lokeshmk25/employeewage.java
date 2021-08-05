58c24fd5c700156510645462d6176b94a1ebd0d3
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-05 11:44:08 +0530

    modified:empcheck

--- src/com/bridgelabz/Empwage.java
+++ src/com/bridgelabz/Empwage.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
 
 public class Empwage {
-    public static void main(String[] args) {
-        System.out.println("welcome to employee wage computation");
-    }
-}
+            public static void main(String[] args) {
+
+                System.out.println("Welcome to Bridgelabz EmployeeWageBuilder");
+//constant
+                int IS_PRESENT = 1;
+
+                int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+                if (empCheck == IS_PRESENT)
+                    System.out.println(" Employee is Present");
+                else
+                    System.out.println(" Employee is Absent");
+            }
+        }
+
+
